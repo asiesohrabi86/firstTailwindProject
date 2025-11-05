4b73331c5725fe4309524244e851b364e4bd9ea3
asiesohrabi <asie.sohrabiii@gmail.com>
2025-11-05 18:23:55 +0330

    script part in package.json has been corrected so that (images,vendor,js,fonts,all.min.css,all.min.js) files could be coppied into the dist folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,10 +24,10 @@ module.exports = {
       },
 
       backgroundImage: {
-        'first-section-header-bg': "url('/src/images/payment_landing.png')",
-        'first-article-img': "url('/src/images/1.jpg')",
-        'second-section-img': "url('/src/images/2.jpg')",
-        'cinema': "url('/src/images/bg-cinema.png')",
+        'first-section-header-bg': "url('../assets/images/payment_landing.png')",
+        'first-article-img': "url('../assets/images/1.jpg')",
+        'second-section-img': "url('../assets/images/2.jpg')",
+        'cinema': "url('../assets/images/bg-cinema.png')",
       },
     },
   },
